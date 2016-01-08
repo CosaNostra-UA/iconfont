925fda11d17edf624e1440953ad8fcd9028e8a02
m_tsymbal <marina.tsymbal@filmon.com>
2016-01-08 17:11:04 +0200

    added tab to generate font

--- src/js/app.js
+++ src/js/app.js
@@ -2,5 +2,24 @@
 
 
 var $ = require('jquery');
+/*var inputData = require('../inputData.json');*/
+
+$('document').ready(function() {
+    $('body').on('click', '.icon', function(){
+        $(this).toggleClass('selected');
+    });
+
+    $('#font').click(function() {
+        $.ajax({
+            url: "test.html",
+            success: function(html){
+                $('#font').addClass('selected');
+                $('.icons-list').hide();
+                $('body').append(html);
+            }
+        });
+    });
+});
+
 
 
